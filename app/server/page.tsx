b2c8d2f24d8ea42ffa6b3a5489febb872dc4c987
syed kashif raza rizvi <kashifr684@gmail.com>
2023-04-27 09:55:48 +0500

    Data fetching

---
+++ app/server/page.tsx
@@ -0,0 +1,36 @@
+import { log } from "console";
+import React from "react";
+
+
+type Book = {
+    id : number;
+    name : string;
+    type : string;
+    available : boolean;
+};
+
+async function getBooks() {
+    const response = await fetch("https://simple-books-api.glitch.me/books", {
+        cache:"no-store",
+    })
+    const data = response.json();
+    return data;
+}
+
+export default async function staticPage() {
+    const books= await getBooks();
+
+
+  return (
+  <div> 
+    <h1>Static page</h1>
+  {books.map((book:Book) =>(
+    <ul key={book.id}>
+    <li>
+        {book.name} - {book.type} 
+    </li>
+    </ul>
+  ))}
+  </div>
+  );
+}
